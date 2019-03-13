b8485bf8b465af3b1d0befff844b2028a36e35e6
zc710189 <1137145922@qq.com>
2019-03-13 17:30:06 +0800

    第一次修改，1.0添加注释并释放数据库，创建2.0

--- ConnectMySQL1.0.py
+++ ConnectMySQL1.0.py
@@ -5,13 +5,13 @@ import re
 
 con = pm.connect(host='localhost', user='root', passwd='123456', charset='utf8')
 cur = con.cursor()
-
+#创建数据库函数
 def Build_db():
     cur.execute("create database RS_db character set utf8;")
     cur.execute("use RS_db;")
     cur.close()
     con.close()
-
+#检测表是否存在的函数
 def Create_table(con, table_name):
     sql = "show tables;"
     con.execute(sql)
@@ -23,6 +23,7 @@ def Create_table(con, table_name):
     else:
         return 0
 
+#检测数据库是否存在的函数
 def Create_databases(db_name):
     sql = "show databases;"
     cur.execute(sql)
@@ -33,34 +34,176 @@ def Create_databases(db_name):
         return 1
     else:
         return 0
-
+#判断数据库是否存在
 db_name = "rs_db"
 if(Create_databases(db_name) != 1):
     Build_db()
+#创建表的sql语句
 problem = """CREATE TABLE PROBLEM (
+             TITLE_NUMBER INT PRIMARY KEY,
              QUESTION_TYPE  CHAR(50) NOT NULL,
              TOTAL_SCORE  INT NOT NULL)"""
 users = """CREATE TABLE USERS (
-             ACCOUNT_NUMBER  CHAR(20) PRIMARY KEY NOT NULL,
+             ACCOUNT_NUMBER  CHAR(20) PRIMARY KEY,
              PASSWORD  CHAR(20) NOT NULL,
              ID  CHAR(10) NOT NULL,
              NAME  CHAR(20),
              AGE INT,  
              SEX CHAR(1) )"""
 Mistaken_questions = """CREATE TABLE MISTAKEN_QUESTIONS (
+             TITLE_NUMBER INT PRIMARY KEY,
              QUESTION_TYPE  CHAR(50) NOT NULL,
              TOTAL_SCORE  INT NOT NULL,
              SCORE INT NOT NULL )"""
+#表名的list
 TablesN=["problem","users","mistaken_questions"]
+#创建表sql语句的list
 Tables=[problem,users,Mistaken_questions]
-
-
+#连接数据库
 connect = pm.connect(host='localhost', user='root', passwd='123456', db='RS_db', charset='utf8')
+#获取光标
 con = connect.cursor()
+#判断表是否存在
 for i in range(len(Tables)):
     if(Create_table(con,TablesN[i]) != 1):
         print("表不存在，添加一张")
         con.execute(Tables[i])
+#查看数据库是否存在某表，存在返回表名，不存在则重新输入
+def FindTable():
+    while True:
+        sql = "show tables;"
+        con.execute(sql)
+        tables = [con.fetchall()]
+        table_list = re.findall('(\'.*?\')', str(tables))
+        table_list = [re.sub("'", '', each) for each in table_list]
+        for i in table_list:
+            print(i)
+        TableName = raw_input("请选择的表名：")
+        if TableName in table_list:
+            return TableName
+        else:
+            print("请重新选择：")
+# 查看表TableName的全部字段，存在字段的list
+def FindAttribute(TableName):
+    sql = "show columns from " + TableName + " from RS_db"
+    FieldNum = con.execute(sql)
+    TableField = [con.fetchall()]
+    temp_list = re.findall('(\'.*?\')', str(TableField))
+    temp_list = [re.sub("'", '', each) for each in temp_list]
+    Attribute = []
+    for i in range(len(temp_list) / 5):
+        Attribute.append(temp_list[i * 5])
+    return Attribute
+#添加函数
+def Add():
+    print("请从以下表中选择添加数据的表：")
+    TableName = FindTable()
+    Attribute = FindAttribute(TableName)
+    Attributes = ""
+    for i in range(len(Attribute)):
+        if 0 != i:
+            Attributes += ","
+        Attributes += Attribute[i]
+    temp = ""
+    for i in range(len(Attribute)):
+        if 0 != i:
+            temp += ","
+        temp += "%s"
+    while True:
+        sql_insert = "INSERT INTO "+TableName+"("+Attributes+") VALUES ("+temp+")"
+        temp_data = []
+        for i in range(len(Attribute)):
+            print("请输入%s："%(Attribute[i]))
+            temp=raw_input("")
+            temp_data.append(temp)
+        try:
+            con.execute(sql_insert, temp_data)
+            connect.commit()
+            print('添加成功！')
+            break
+        except Exception as e:
+            print('出错回滚完成', e)
+            connect.rollback()
+            con.close()
+            connect.close()
+#删除函数
+def Delelt():
+    print("请从以下表中选择删除数据的表：")
+    TableName = FindTable()
+    print("请从以下属性中选择：")
+    Attribute = FindAttribute(TableName)
+    for i in Attribute:
+        print(i)
+    SelAttribute = raw_input("请选择属性：")
+    AttributeValue = raw_input("请选择属性值：")
+    sql_delete = "DELETE FROM "+TableName+" where "+SelAttribute+"='"+AttributeValue+"'"
+    con.execute(sql_delete)
+#查询函数
+def Lookup():
+    print("请从以下表中选择查看数据的表：")
+    TableName = FindTable()
+    sql_select = "SELECT * FROM "+TableName
+    Attribute = FindAttribute(TableName)
+    num = con.execute(sql_select)
+    data_all = con.fetchall()
+    for i in range(num+1):
+        for j in range(len(Attribute)):
+            if 0==i:
+                print(Attribute[j])
+            else:
+                print(data_all[i-1][j])
+#更改函数
+def Change():
+    print("请从以下表中选择修改数据的表：")
+    TableName = FindTable()
+    print("请从以下属性中选择：")
+    Attribute = FindAttribute(TableName)
+    for i in Attribute:
+        print(i)
+    while True:
+        LookupSelAttribute = raw_input("请选择查找属性：")
+        LookupAttributeValue = raw_input("请选择查找属性值：")
+        sql_Lookup = "SELECT * FROM "+TableName+" WHERE "+LookupSelAttribute+"='"+LookupAttributeValue+"'"
+        if 0==con.execute(sql_Lookup):
+            print("没有该字段！请重新输入")
+            continue
+        else:
+            break
+    ModSelAttribute = raw_input("请选择修改属性：")
+    ModAttributeValue = raw_input("请选择修改属性值：")
+    sql_update = "UPDATE "+TableName+" SET "+ModSelAttribute+"="+ModAttributeValue+" WHERE "+LookupSelAttribute+"='"+LookupAttributeValue+"'"
+    con.execute(sql_update)
+#循环
+while True:
+    print("1.添加")
+    print("2.删除")
+    print("3.查找")
+    print("4.修改")
+    print("5.退出")
+    num=int(input("请输入1-5的数字选择功能："))
+    if 1==num:
+        Add()
+    elif 2==num:
+        Delelt()
+    elif 3==num:
+        Lookup()
+    elif 4==num:
+        Change()
+    elif 5==num:
+        break
+    else:
+        print("请输入1-5的数字！")
+#为了写2.0删除表和数据库
+sql = "show tables;"
+con.execute(sql)
+tables = [con.fetchall()]
+table_list = re.findall('(\'.*?\')', str(tables))
+table_list = [re.sub("'", '', each) for each in table_list]
+for i in table_list:
+    delsql="DROP TABLE "+i
+    con.execute(delsql)
+delsql="DROP DATABASE "+db_name
+con.execute(delsql)
 #test部分
 """sqladd1 = "INSERT INTO PROBLEM(QUESTION_TYPE, TOTAL_SCORE) VALUES ('chen', 10)"
 sqladd2 = "INSERT INTO PROBLEM(QUESTION_TYPE, TOTAL_SCORE) VALUES ('zhen', 20)"
@@ -103,123 +246,3 @@ con.execute(sqldel1)
 con.execute(sqldel2)
 con.execute(sqldel3)"""
 #test部分
-
-def FindTable():
-    while True:
-        sql = "show tables;"
-        con.execute(sql)
-        tables = [con.fetchall()]
-        table_list = re.findall('(\'.*?\')', str(tables))
-        table_list = [re.sub("'", '', each) for each in table_list]
-        for i in table_list:
-            print(i)
-        TableName = raw_input("请选择的表名：")
-        if TableName in table_list:
-            return TableName
-        else:
-            print("请重新选择：")
-def FindAttribute(TableName):
-    sql = "show columns from " + TableName + " from RS_db"
-    FieldNum = con.execute(sql)
-    TableField = [con.fetchall()]
-    temp_list = re.findall('(\'.*?\')', str(TableField))
-    temp_list = [re.sub("'", '', each) for each in temp_list]
-    Attribute = []
-    for i in range(len(temp_list) / 5):
-        Attribute.append(temp_list[i * 5])
-    return Attribute
-def Add():
-    print("请从以下表中选择添加数据的表：")
-    TableName = FindTable()
-    Attribute = FindAttribute(TableName)
-    Attributes = ""
-    for i in range(len(Attribute)):
-        if 0 != i:
-            Attributes += ","
-        Attributes += Attribute[i]
-    temp = ""
-    for i in range(len(Attribute)):
-        if 0 != i:
-            temp += ","
-        temp += "%s"
-    while True:
-        sql_insert = "INSERT INTO "+TableName+"("+Attributes+") VALUES ("+temp+")"
-        temp_data = []
-        for i in range(len(Attribute)):
-            print("请输入%s："%(Attribute[i]))
-            temp=raw_input("")
-            temp_data.append(temp)
-        try:
-            con.execute(sql_insert, temp_data)
-            connect.commit()
-            print('添加成功！')
-            break
-        except Exception as e:
-            print('出错回滚完成', e)
-            connect.rollback()
-            con.close()
-            connect.close()
-def Delelt():
-    print("请从以下表中选择删除数据的表：")
-    TableName = FindTable()
-    print("请从以下属性中选择：")
-    Attribute = FindAttribute(TableName)
-    for i in Attribute:
-        print(i)
-    SelAttribute = raw_input("请选择属性：")
-    AttributeValue = raw_input("请选择属性值：")
-    sql_delete = "DELETE FROM "+TableName+" where "+SelAttribute+"='"+AttributeValue+"'"
-    con.execute(sql_delete)
-def Lookup():
-    print("请从以下表中选择查看数据的表：")
-    TableName = FindTable()
-    sql_select = "SELECT * FROM "+TableName
-    con.execute(sql_select)
-    data_all = con.fetchall()
-    data_list = re.findall('(\'.*?\')', str(data_all))
-    data_list = [re.sub("'", '', each) for each in data_list]
-    print(data_all)
-    print(data_list)
-def Change():
-    print("请从以下表中选择修改数据的表：")
-    TableName = FindTable()
-    print("请从以下属性中选择：")
-    Attribute = FindAttribute(TableName)
-    for i in Attribute:
-        print(i)
-    while True:
-        LookupSelAttribute = raw_input("请选择查找属性：")
-        LookupAttributeValue = raw_input("请选择查找属性值：")
-        sql_Lookup = "SELECT * FROM "+TableName+" WHERE "+LookupSelAttribute+"='"+LookupAttributeValue+"'"
-        if 0==con.execute(sql_Lookup):
-            print("没有该字段！请重新输入")
-            continue
-        else:
-            break
-    ModSelAttribute = raw_input("请选择修改属性：")
-    ModAttributeValue = raw_input("请选择修改属性值：")
-    sql_update = "UPDATE "+TableName+" SET "+ModSelAttribute+"="+ModAttributeValue+" WHERE "+LookupSelAttribute+"='"+LookupAttributeValue+"'"
-    con.execute(sql_update)
-
-
-while True:
-    print("1.添加")
-    print("2.删除")
-    print("3.查找")
-    print("4.修改")
-    print("5.退出")
-    num=int(input("请输入1-5的数字选择功能："))
-    if 1==num:
-        Add()
-    elif 2==num:
-        Delelt()
-    elif 3==num:
-        Lookup()
-    elif 4==num:
-        Change()
-    elif 5==num:
-        break
-    else:
-        print("请输入1-5的数字！")
-
-
